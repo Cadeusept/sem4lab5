20e22f39fe91832ad8e607be2fbd111423d86093
Cadeusept <matwey.likhota@gmail.com>
2021-11-23 12:20:44 +0300

    work in progress

--- include/stack.hpp
+++ include/stack.hpp
@@ -5,4 +5,38 @@
 
 auto example() -> void;
 
+template <typename T>
+class Stack
+{
+public:
+    void push(T&& value);
+
+    void push(const T& value) {
+        auto old_el = current;
+        current = new StackElement();
+        current->prev=old_el;
+        current->value=value;
+    };
+
+    void pop() {
+        if (current->prev != nullptr) {
+            auto tmp=current;
+            current=current->prev;
+            delete tmp;
+        }
+    };
+
+    const T& head() const{
+        return current->value;
+    };
+
+private:
+    struct StackElement{
+        StackElement* prev;
+        T value;
+        StackElement() {
+        }
+    } *current;
+};
+
 #endif // INCLUDE_STACK_HPP_
